dd37b13f7374140349051349f0db2bcc972f6847
Lixiu520 <118369717+Lixiu520@users.noreply.github.com>
2023-04-30 15:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 
+// Tout d'abord, je cree une class Todo représente un todo : avec sont titre, description et status(dans la constructeur)
+class Todo {
+    constructor(title, description,status) {
+      this.title = title;
+      this.description = description;
+      this.status = status; 
+    }
+  
+  }
+//  L'événement DOMContentLoaded se produit lorsque le document HTML initial a été complètement chargé et analysé
+//  Le code qui suit sera exécuté uniquement lorsque cet événement se produit
+  document.addEventListener("DOMContentLoaded", function(){
+    // pour collecter les infos puis les stocker dans localstorage
+  const form = document.querySelector('form');
+  const todoList = document.querySelector(".todoList");
+ 
+  let todos = JSON.parse(localStorage.getItem('todos')) ;
+  if(!todos){
+    todos = [];
+    const todo1 = new Todo("How to use the application TODOS","click the plus to add a new task, once the task is created, it goes to the top of the todo list. ",false)
+    const todo2 = new Todo("If the task is marked as completed","When the task is completed, it will be crossed and moved to the bottom of the list.",false);
+    todos.push(todo1);
+    todos.push(todo2);
+    localStorage.setItem('todos', JSON.stringify(todos));
+} 
+
+ 
+  console.log(todos)
+// pour recuperer les infos à partir d'un formulaire: create
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const status = document.getElementById('status').checked;
+    const todo = new Todo(title, description, status);
+    
+    todos.push(todo);
+
+ localStorage.setItem('todos', JSON.stringify(todos));
+    
+    displayTodoList();
+
+    window.location.href = window.location.href;
+
+    form.reset();
+   
+  });
+
+ 
+// pour afficher la list de todos
+
+function displayTodoList(){
+    if (todoList) {
+        todos.sort(function(a, b) {
+            return b.status - a.status || b.index - a.index;
+            // pour que les taches effectué soient restés bas de la list  
+        });
+        for(var i = todos.length-1; i>=0; i--){
+          // pour afficher le nouveau tache en haut de la list
+            const item = `
+               <li class="seperation " id="${todos[i].title}">
+                
+                     <div class="container containerRow">    
+                     <a href="detail.html#${todos[i].title}" class="alink" data-title = "${todos[i].title}">  
+                         <input type="checkbox" name="validation" ${todos[i].status ? 'checked':''}>
+                         <label class="flexItem2 todoTitle"for="validation">${todos[i].title}</label>	
+                     </a>   
+                         <img class="flexItem3 delete" src="img/delete.png" alt="Delete" >
+                     </div>   
+                            
+               </li>
+            `;
+            todoList.innerHTML += item;
+        };
+    }
+ }
+displayTodoList();
