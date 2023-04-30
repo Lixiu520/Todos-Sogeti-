e94a571ba9673f8cbf96d0c87c97bf955095728e
Lixiu520 <118369717+Lixiu520@users.noreply.github.com>
2023-04-30 15:30:49 +0200

    Update detail.js

--- detail.js
+++ detail.js
@@ -10,10 +10,11 @@ class Todo {
     }
   
   }
-const  todos = JSON.parse(localStorage.getItem('todos')) ;
+const  todos = JSON.parse(localStorage.getItem('todos')) ;//pour recuperer les donné sur localstorage
 console.log(todos);
 const todo1 = new Todo("How to use the application TODOS","click the plus to add a new task, once the task is created, it goes to the top of the todo list. ",false)
 const todo2 = new Todo("If the task is marked as completed","When the task is completed, it will be crossed and moved to the bottom of the list.",false);
+
 const  detail = document.querySelector(".detail");
 
 function displayDetails(){
@@ -23,6 +24,7 @@ function displayDetails(){
             if (window.location.hash) {
                 // get the fragment identifier
                 var fragment = window.location.hash.substring(1); // remove the leading #
+                //le window.location.hash montre pas celui de deux taches que j'ai initialisé, j'ai du le faire endure
                 if(fragment === "How%20to%20use%20the%20application%20TODOS"){
                     const item =` 
                   
@@ -50,6 +52,7 @@ function displayDetails(){
                   `;
                     detail.innerHTML = item;
                 }
+                // mais les autres que j'ai ajouté à partir de formulaire, ils affichent bien dans le page detail 
                 for(var i = 0; i<todos.length; i++){
                     console.log("looping through todos");
                 // do something with the fragment identifier
